68ecd4e6643e366eb23076c57becb6fddfaa4eb4
muhammad <muhammadthahir6@gmail.com>
2018-02-25 21:42:22 +0530

    added some intent

--- app/src/main/java/com/example/raju/lovelysmiles/Login.java
+++ app/src/main/java/com/example/raju/lovelysmiles/Login.java
@@ -1,13 +1,40 @@
 package com.example.raju.lovelysmiles;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class Login extends AppCompatActivity {
+    public Button button;
+    public TextView newuser;
+    Intent i;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        button =(Button) findViewById(R.id.button_login);
+        newuser=(TextView)findViewById(R.id.newuser);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                i=new Intent(Login.this,Home.class);
+                Toast.makeText(Login.this, "Logged in", Toast.LENGTH_SHORT).show();
+                startActivity(i);
+            }
+        });
+
+        newuser.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                i=new Intent(Login.this,Signup.class);
+                startActivity(i);
+            }
+        });
     }
 }
--- app/src/main/java/com/example/raju/lovelysmiles/Signup.java
+++ app/src/main/java/com/example/raju/lovelysmiles/Signup.java
@@ -1,13 +1,27 @@
 package com.example.raju.lovelysmiles;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class Signup extends AppCompatActivity {
-
+    public Button SignupBtn;
+    public Intent signIntent;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
+        SignupBtn=(Button)findViewById(R.id.button_signup);
+        signIntent=new Intent(Signup.this,Login.class);
+        SignupBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(Signup.this, "Account Created Successfully", Toast.LENGTH_SHORT).show();
+                startActivity(signIntent);
+            }
+        });
     }
 }
