96c910593be36b110f77d827e3492ba7c0972b33
athulraj2002 <athulraj2002@gmail.com>
2018-02-26 00:23:25 +0530

    Registration2 Completed

---
+++ app/src/main/java/com/example/raju/lovelysmiles/Registration2.java
@@ -0,0 +1,13 @@
+package com.example.raju.lovelysmiles;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Registration2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registration2);
+    }
+}
