37e8c707125d11fa60fc39a6a8fb964efb02b459
athulraj2002 <athulraj2002@gmail.com>
2018-02-26 20:47:05 +0530

    Registration3 Completed , added new border4 and "changed background"

---
+++ app/src/main/java/com/example/raju/lovelysmiles/registration3.java
@@ -0,0 +1,13 @@
+package com.example.raju.lovelysmiles;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class registration3 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registration3);
+    }
+}
