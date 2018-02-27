d61c6ef1cddf58e12d53ffca55a2f4c27c490027
muhammad <muhammadthahir6@gmail.com>
2018-02-27 06:24:39 +0530

    r3 and paymentt

--- app/src/main/java/com/example/raju/lovelysmiles/payment.java
+++ app/src/main/java/com/example/raju/lovelysmiles/payment.java
@@ -1,12 +1,18 @@
 package com.example.raju.lovelysmiles;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageButton;
 import android.widget.LinearLayout;
+import android.widget.TextView;
 
 public class payment extends AppCompatActivity {
     LinearLayout ll1,ll2,ll3,ll4;
+    ImageButton pbw;
+    Intent canint;
+    TextView cancel;
     private void noselect(){
         ll1.setBackground(getDrawable(R.drawable.noselect));
         ll2.setBackground(getDrawable(R.drawable.noselect));
@@ -19,6 +25,22 @@ public class payment extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_payment);
 
+        canint=new Intent(payment.this,Home.class);
+        pbw=(ImageButton)findViewById(R.id.pbw);
+        pbw.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
+        cancel=(TextView)findViewById(R.id.cancel);
+        cancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+                startActivity(canint);
+            }
+        });
         ll1=(LinearLayout)findViewById(R.id.ll1);
         ll2=(LinearLayout)findViewById(R.id.ll2);
         ll3=(LinearLayout)findViewById(R.id.ll3);
--- app/src/main/java/com/example/raju/lovelysmiles/registration3.java
+++ app/src/main/java/com/example/raju/lovelysmiles/registration3.java
@@ -5,15 +5,24 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 
 public class registration3 extends AppCompatActivity {
     Button paymentbtn;
+    ImageButton r3bw;
     Intent r3int;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registration3);
 
+        r3bw=(ImageButton)findViewById(R.id.r3bw);
+        r3bw.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
         paymentbtn=(Button)findViewById(R.id.buttton_pay);
         r3int=new Intent(registration3.this, payment.class);
         paymentbtn.setOnClickListener(new View.OnClickListener() {
